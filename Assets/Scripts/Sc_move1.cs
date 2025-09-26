cc2cc03cbd98481b03322a508f3ef7edbe08934d
Rafael Nicholas Po <163509148+rafpoo@users.noreply.github.com>
2025-09-26 14:37:52 +0700

    feat: challenge 1

--- Assets/Scripts/Sc_move1.cs
+++ Assets/Scripts/Sc_move1.cs
@@ -6,10 +6,14 @@ public class Sc_move1 : MonoBehaviour
 {
     private float kecepatan = 5f;
     private float putaran = 5f;
+    private float kecepatanLoncat = 10f;
+    private Rigidbody rb;
+
+    public bool isGrounded = true;
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -18,7 +22,21 @@ public class Sc_move1 : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * kecepatanLoncat, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
         transform.Translate(new Vector3(0, 0, v) * kecepatan * Time.deltaTime);
         transform.Rotate(new Vector3(0, h, 0) * putaran);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("land"))
+        {
+            isGrounded = true;
+        }
     }
 }
